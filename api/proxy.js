bb9392c397c0839a957fa681978d5fa0b658ccc3
H-e-l-l-oW-o-r-l-d <33751906+H-e-l-l-oW-o-r-l-d@users.noreply.github.com>
2023-06-28 18:29:18 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,5 +1,24 @@
 const { createProxyMiddleware, responseInterceptor } = require('http-proxy-middleware');
 
+const proxy = createProxyMiddleware({
+  target: "https://dns.google/",
+  changeOrigin: true,
+  //selfHandleResponse: true,
+  pathRewrite: {
+    // rewrite request path `/backend`
+    //  /backend/user/login => http://google.com/user/login
+    //   "^/backend/": "/",
+  }/*,
+  on: {
+    proxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+      //res.statusCode = 200; // set different response status code
+      const response = responseBuffer.toString('utf8');
+      return response;
+    }),
+  }
+    */
+});
+
 module.exports = (req, res) => {
   //   if (
   //     req.url.startsWith("/api") ||
@@ -9,22 +28,5 @@ module.exports = (req, res) => {
   //   ) {
   //     target = "http://106.15.2.32:6969";
   //   }
-  createProxyMiddleware({
-    target: "https://dns.google/",
-    changeOrigin: true,
-    //selfHandleResponse: true,
-    pathRewrite: {
-      // rewrite request path `/backend`
-      //  /backend/user/login => http://google.com/user/login
-      //   "^/backend/": "/",
-    }/*,
-    on: {
-      proxyRes: responseInterceptor(async (responseBuffer, proxyRes, req2, res2) => {
-        //res2.statusCode = 200; // set different response status code
-        const response = responseBuffer.toString('utf8');
-        return response;
-      }),
-    }
-      */
-  })(req, res);
+  proxy(req, res);
 };
