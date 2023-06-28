ff1b402cc5683ca2ac4e07921e0481ddce487841
H-e-l-l-oW-o-r-l-d <33751906+H-e-l-l-oW-o-r-l-d@users.noreply.github.com>
2023-06-28 18:37:10 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -3,20 +3,19 @@ const { createProxyMiddleware, responseInterceptor } = require('http-proxy-middl
 const proxy = createProxyMiddleware({
   target: "https://dns.google/",
   changeOrigin: true,
-  //selfHandleResponse: true,
+  selfHandleResponse: true,
   pathRewrite: {
     // rewrite request path `/backend`
     //  /backend/user/login => http://google.com/user/login
     //   "^/backend/": "/",
-  }/*,
+  },
   on: {
     proxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
-      //res.statusCode = 200; // set different response status code
+      res.statusCode = 200; // set different response status code
       const response = responseBuffer.toString('utf8');
       return response;
     }),
   }
-    */
 });
 
 module.exports = (req, res) => {
