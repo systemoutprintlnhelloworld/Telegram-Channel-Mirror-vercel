830c3ee7b7dfb9946327abf7a8d5f785144be20b
H-e-l-l-oW-o-r-l-d <33751906+H-e-l-l-oW-o-r-l-d@users.noreply.github.com>
2023-06-29 16:01:27 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -21,12 +21,13 @@ const proxy = createProxyMiddleware({
     res.setHeader('Access-Control-Allow-Origin', '*');
     if (proxyRes.headers['content-type'].includes('text/html')) {
       const root = parse(responseBuffer);
+      const title = root.getElementsByTagName("title")[0];
+      title.innerHTML = title.innerHTML.replaceAll('Google Public DNS', 'Uchan Public DNS');
       tryDel(root, ".logo");
       tryDel(root, ".help");
       tryDel(root, 'a[href="https://developers.google.com/speed/public-dns/docs/using"]');
       tryDel(root, 'a[href="https://developers.google.com/speed/public-dns"]');
       tryDel(root, 'link[rel="shortcut icon"]'); // icon
-      console.log(res);
       return root.toString('utf8').replaceAll("https://dns.google/resolve?", "https://" + req.headers['x-forwarded-host'] + "/resolve?");
     } else {
       console.log(proxyRes.headers['content-type'])
